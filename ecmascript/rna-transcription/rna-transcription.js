acc8ba60dd12e70e360b6ebbbe9fc3ffd813ef33
Elijah McClendon <Elijah@fungry.com>
2018-02-24 15:33:52 -0700

    RNA Transcription - Setup (003) Complete

--- ecmascript/rna-transcription/rna-transcription.js
+++ ecmascript/rna-transcription/rna-transcription.js
@@ -1,3 +1,16 @@
-/**
- * Created by Elijah McClendon on 2/24/18.
- */
+let Transcriptor = function (){};
+
+Transcriptor.prototype.toRna = function(input){
+
+  let dna = input.split('');
+  for(let i = 0; i < dna.length; i++){
+    dna[i] === 'C' ? dna[i] = 'G' :
+        dna[i] === 'G' ? dna[i] = 'C' :
+            dna[i] === 'A' ? dna[i] = 'U' :
+                dna[i] === 'T' ? dna[i] = 'A' :
+                    (function(){throw 'Invalid input DNA.'}());
+  }
+  return dna.join('');
+};
+
+module.exports = Transcriptor;
